abc4e35380c549743e67e6292fa541a892b3fd57
hassam361 <hassam.asif48@gmail.com>
2020-03-04 21:15:32 +0500

    dfd

--- demand_drive/urls.py
+++ demand_drive/urls.py
@@ -3,10 +3,17 @@ from django.contrib import admin
 from django.urls import path
 from website import views as web_views
 from users import views as user_views
+
 from django.contrib.auth import views as auth_views 
+
+from website import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', web_views.home, name='home'),
+    path('demand/<int:pk>/', web_views.DemandDetailView.as_view(), name='demand-detail'),
+    path('', web_views.DemandListView.as_view(), name='home'),
+    path('demand/new/', web_views.DemandCreateView.as_view(), name='demand-create'),
+    path('demand/<int:pk>/update/', web_views.DemandUpdateView.as_view(), name='demand-update'),
+    path('demand/<int:pk>/delete/', web_views.DemandDeleteView.as_view(), name='demand-delete'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('register/',user_views.register,name='register'),
--- website/views.py
+++ website/views.py
@@ -1,8 +1,59 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from .models import Demand
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 # Create your views here.
 def home(request):
-    
+    context = {
+        'demands': Demand.objects.all()
+    }
     
     return render(request, 'website/index.html')
+
+class DemandListView(ListView):
+    model = Demand
+    template_name = 'website/index.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'demands'
+    ordering = ['-date_posted']
+    paginate_by = 5
+class DemandDetailView(DetailView):
+    model = Demand
+class DemandCreateView(LoginRequiredMixin, CreateView):
+    model = Demand
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+class DemandUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Demand
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        demand = self.get_object()
+        if self.request.user == demand.author:
+            return True
+        return False
+
+
+class DemandDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Demand
+    success_url = '/'
+
+    def test_func(self):
+        demand = self.get_object()
+        if self.request.user == demand.author:
+            return True
+        return False
